a7d225262864940fabf8feb4ff996c2db6b4772d
DaniDanyDani <daniel.moreira.leme@gmail.com>
2025-05-06 16:00:48 -0300

    só falta ajustar a tag do volume e adicionar fibrose

--- main_teste.py
+++ main_teste.py
@@ -3,7 +3,7 @@ import math
 import os
 import sys
 
-filename = "Patient_10"
+filename = "Patient_7"
 
 gmsh.initialize()
 gmsh.clear()
@@ -18,39 +18,58 @@ gmsh.option.setNumber("Mesh.CharacteristicLengthMax", 2.25)
 gmsh.model.add("TestModel")
 
 path = os.path.dirname(os.path.abspath(__file__))
-gmsh.merge(os.path.join(path, 'epi.stl'))
-gmsh.merge(os.path.join(path, 'lv.stl'))
-gmsh.merge(os.path.join(path, 'rv.stl'))
-gmsh.merge(os.path.join(path, 'base.stl'))
+gmsh.merge(os.path.join(path, 'epi0.stl'))
+gmsh.merge(os.path.join(path, 'lv0.stl'))
+gmsh.merge(os.path.join(path, 'rv0.stl'))
+gmsh.merge(os.path.join(path, 'base0.stl'))
 
 gmsh.model.mesh.removeDuplicateNodes()
 gmsh.model.mesh.refine()
 
 includeBoundary = True
 forceParametrizablePatches=False
-angle = 40
+angle = 50
 
 curveAngle = 180
 
 gmsh.model.mesh.classifySurfaces(angle * math.pi / 180., includeBoundary,
                                 forceParametrizablePatches,
                                 curveAngle * math.pi / 180.)
-# curveAngle * math.pi / 180.
 
 gmsh.model.mesh.createGeometry()
 
 surfaces = gmsh.model.getEntities(dim=2)
-print(f"Superfícies detectadas: {len(surfaces)}")
+# print(f"Superfícies detectadas: {len(surfaces)}")
 
 surface_tags = [s[1] for s in surfaces]
-loop = gmsh.model.geo.addSurfaceLoop(surface_tags)
-volume = gmsh.model.geo.addVolume([loop])
+loop = gmsh.model.geo.addSurfaceLoop(surface_tags, 1)
+volume = gmsh.model.geo.addVolume([loop], 2)
 
+volumes = gmsh.model.getEntities(dim=3)
+volume_tags = [v[1] for v in volumes]
 
 gmsh.model.geo.synchronize()
 
-gmsh.model.mesh.generate(3)
-gmsh.write(filename + ".msh")
+
+if len(surface_tags) == 4:
+    print(f"{surface_tags=}")
+    print(f"{loop=}")
+    gmsh.model.addPhysicalGroup(2, [surface_tags[0]], 40, name="epi")
+    gmsh.model.addPhysicalGroup(2, [surface_tags[1]], 20, name="ve")
+    gmsh.model.addPhysicalGroup(2, [surface_tags[2]], 30, name="vd")
+    gmsh.model.addPhysicalGroup(2, [surface_tags[3]], 10, name="base")
+    gmsh.model.addPhysicalGroup(3, [volume], 1, name="healthy")
+    gmsh.model.mesh.generate(3)
+    gmsh.write(filename + ".msh")
+else:
+    print("Atenção, número de superfícies está errado, gerando volume sem physical groups")
+    print(f"{surface_tags=}")
+    print(f"{volume_tags=}")
+    gmsh.model.mesh.generate(3)
+    gmsh.write(filename + ".msh")
+    
+
+
 
 
 
---
+++ malha-fibrose.geo
@@ -0,0 +1,36 @@
+Mesh.CharacteristicLengthMin = 0.75;
+Mesh.CharacteristicLengthMax = 2.25;
+
+Merge "epi.stl";
+Merge "lv.stl";
+Merge "rv.stl";
+Merge "base.stl";
+
+Coherence Mesh;
+
+RefineMesh;
+
+CreateTopology;
+
+
+CreateGeometry;
+
+// Merge "fibrose.stl";
+
+Surface Loop(1) = {1, 2, 3, 4};
+
+Physical Surface("base", 10) = {4};
+Physical Surface("epi", 40) = {1};
+Physical Surface("ve", 30) = {2};
+Physical Surface("vd", 20) = {3};
+// Physical Surface("septo", 50) = {5};
+
+Volume(1) = {1};
+
+// Surface Loop(2) = {5};
+
+// Volume(2) = {2};
+
+Physical Volume("healthy", 1) = {1};
+
+// Physical Volume("fibrose", 2) = {2};
